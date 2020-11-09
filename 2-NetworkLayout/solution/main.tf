97421371780796bf553bf4f2c5c0cba458580ba6
Alex Lucas <arglucas@hotmail.com>
2020-11-09 20:00:24 +0000

    Provided solution 2 and initial state for solution 3.

---
+++ 2-NetworkLayout/solution/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = var.region
+    profile = var.profile
+}
+
+module "network" {
+    source = "./network"
+}
+
+module "security" {
+    source = "./security"
+    vpc_id = module.network.vpc_id
+}
+
+module "compute" {
+    source          = "./compute"
+    publicSubnetA   = module.network.publicSubnetA
+    appA            = module.network.appA
+    appB            = module.network.appB
+    appC            = module.network.appC
+    BastionSG       = module.security.BastionSG
+    AppSG           = module.security.AppSG
+}
